52b4674c269d344b056be070e401d01b7ec5eb2a
shrewded <87589442+slimreap@users.noreply.github.com>
2023-05-04 22:33:25 +0800

    05-04-23

--- resources/views/components/products/productcategory.blade.php
+++ resources/views/components/products/productcategory.blade.php
@@ -1,23 +1,14 @@
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-purple">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="{{route('productspccomponents')}}">PC Components</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('productslaptops')}}">Laptops</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('productsdesktoppackages')}}">Desktop Packages</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('productspheriperals')}}">Peripherals and Accessories</a>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link" href="{{route('productspccomponents')}}">PC Components</a>
+      <a class="nav-link" href="{{route('productslaptops')}}">Laptops</a>
+      <a class="nav-link" href="{{route('productsdesktoppackages')}}">Desktop Packages</a>
+      <a class="nav-link" href="{{route('productspheriperals')}}">Peripherals and Accessories</a>
     </div>
-  </nav>
+  </div>
 </div>
+</nav>
