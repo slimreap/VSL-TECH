64d4451c8d4e4c37dec617102043dac94de960d8
shrewded <87589442+slimreap@users.noreply.github.com>
2023-05-04 23:52:27 +0800

    fgfddfhgh

--- resources/views/components/products/productcategory.blade.php
+++ resources/views/components/products/productcategory.blade.php
@@ -5,7 +5,16 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-link" href="{{route('productspccomponents')}}">PC Components</a>
-      <a class="nav-link" href="{{route('productslaptops')}}">Laptops</a>
+      <!-- dropdown for laptop -->
+      <div class="dropdown"> 
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="laptopDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Laptops
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end " aria-labelledby="navbarScrollingDropdown">
+          <li><a class="a txt-dark dropdown-item font-reg" style="font-size: 18px;" href="">Apple</a></li>
+        </ul>
+      </div>
+
       <a class="nav-link" href="{{route('productsdesktoppackages')}}">Desktop Packages</a>
       <a class="nav-link" href="{{route('productspheriperals')}}">Peripherals and Accessories</a>
     </div>
